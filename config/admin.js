0ee6122de206e92df384274ba31eece97fa96c2e
Meher-StolenPad <meher.jouini@stolenpad-studio.com>
2024-03-13 22:32:47 +0100

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'PAnUDDh62kcSiXDYZ+tjJg=='), // Use env('ENV_VARIABLE_NAME', 'default_value') to retrieve the environment variable or a default value
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
