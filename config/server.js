ada417ec0ddedb618e9bedabce4cb77e07e0efa4
Meher-StolenPad <meher.jouini@stolenpad-studio.com>
2024-03-13 22:43:50 +0100

    Update server.js

--- config/server.js
+++ config/server.js
@@ -2,7 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      '2u25f3vsP3QUkPRytc7TDg==',
+      '2LX18fqFpmjBhBTDK9ZEGg==',
+      'e22IjHqQVfHEzK23oSMCqw==',
+      'rdoBNGsF3IcyyjSjzmTSNQ=='
+    ]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
