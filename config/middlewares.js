f6dc943e4e46081df4e51c9c21306ec8cc2d9966
Sioud <sioud.zied.11@gmail.com>
2024-03-15 18:13:55 +0100

    Allow cors fix

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,20 +2,18 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+      name: 'strapi::cors',
+      config: {
+        enabled: true,
+        header: '*',
+        origin: true
+      }
+    }
 ];
-
-module.exports = {
-  settings: {
-    cors: {
-      enabled: true,
-      origin: true,
-    },
-  },
-};
